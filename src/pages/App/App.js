e9030ae3090b18dd053c6cc7fff80b9694549646
David Sheinbein <davesheinbein@gmail.com>
2020-11-12 20:02:42 -0800

    update to table

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -168,8 +168,13 @@ function App() {
 				<CardContent>
 					<h3>Live Cases by Country</h3>
 					<Table countries={tableData} />
-					<h3 className='app__graphTitle'>Worldwide new {casesType}</h3>
-					<LineGraph className='app__graph' casesType={casesType} />
+					<h3 className='app__graphTitle'>
+						Worldwide new {casesType}
+					</h3>
+					<LineGraph
+						className='app__graph'
+						casesType={casesType}
+					/>
 				</CardContent>
 			</Card>
 		</div>
